df4eb4bbd6585436749cbbfcdab37f18a72ea33e
optimuhs <optimuhs7@gmail.com>
2023-05-17 22:42:56 -0500

    update footer text

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,18 +5,15 @@ import { FunctionComponent } from "react";
 
 const Contact:FunctionComponent = () => {
     return(
-            <section id={"#Contact"}className={clsx("m-[20px]", "flex", "flex-col", "content-center","items-center")}>
+            <section id={"#Contact"}className={clsx("m-[20px]", "flex", "flex-col","justify-center", "text-center","items-center")}>
                 <p className={clsx("my-2")}>Find me on these social applications:</p>
-                <div className={clsx( "flex", "flex-row", "justify-center", "space-x-5", "my-5")}>    
+                <div className={clsx( "flex", "flex-row", "justify-center",  "space-x-5", "my-5")}>    
 
                     <a href="https://github.com/Optimuhs"><p><FontAwesomeIcon icon={faGithub} className={"fa-2x"}/></p></a>
                     <a href="https://www.linkedin.com/in/fran-mtz/"><p><FontAwesomeIcon icon={faLinkedin} className={"fa-2x"}/></p></a>
                 </div>
                 
-                <p> or email me directly to ... optimuhs7@gmail.com</p>
-
-                
-
+                <p className={clsx( "flex", "flex-row", "justify-center",)}> or email me directly to ... optimuhs7@gmail.com</p>
             </section>
     )
 }
